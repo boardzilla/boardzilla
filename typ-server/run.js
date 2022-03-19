8490d8a46f75b26c6a32ed13e91b3b38c33267e8
Josh Hull <josh@fireflop.com>
2022-03-19 22:53:13 +0100

    use package.json at root

--- typ-server/run.js
+++ typ-server/run.js
@@ -1,7 +1,8 @@
 const createServer = require('./server')
 const redisUrl = process.env.REDIS_URL || "redis://localhost:6379"
 const secretKey = process.env.SECRET_KEY || "some secret"
-
+const port = parseInt(process.env.PORT || 3000)
 process.env.NODE_ENV = 'development'
+
 const server = createServer({redisUrl, secretKey/* , name: 'local game', path: '/Users/ahull/typ-server/test/fixtures/numberGuesser/' */})
-server.listen(3000)
+server.listen(port)
