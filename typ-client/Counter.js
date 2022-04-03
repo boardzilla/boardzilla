e51ef232cb495c8b57cfae692a82472d67397fbc
Andrew Hull <aghull@gmail.com>
2022-04-02 23:29:53 -0400

    better responsiveness on counters

--- typ-client/Counter.js
+++ typ-client/Counter.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 export default ({id, display, value, min, max, gameAction}) => {
   const [override, setOverride] = useState();
   const set = n => {
-    let newValue = Math.max(min, value + n);
+    let newValue = Math.max(min, (override === undefined ? value : override) + n);
     if (max > 0 && newValue > max) newValue = max;
     setOverride(newValue); // very optimistic update. can't detect if any error yet
     gameAction('setCounter', `"${id}"`, newValue);
@@ -11,7 +11,7 @@ export default ({id, display, value, min, max, gameAction}) => {
   return (
     <div>
       <button onClick={e => {set(-1); e.stopPropagation()}} onTouchEnd={() => set(-1)}>-</button>
-      {display(override || value)}
+      {display(override === undefined ? value : override)}
       <button onClick={e => {set(1); e.stopPropagation()}} onTouchEnd={() => set(1)}>+</button>
     </div>
   );
