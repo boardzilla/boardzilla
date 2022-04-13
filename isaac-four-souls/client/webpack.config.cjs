380518e67ce2ed6bff17170ceeba7e202905bef9
Joshua Hull <joshbuddy@gmail.com>
2022-04-13 13:30:03 +0200

    Move everything webpack related to root

--- isaac-four-souls/client/webpack.config.cjs
+++ isaac-four-souls/client/webpack.config.cjs
@@ -29,7 +29,7 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           "style-loader",
-          "css-loader?url=false",
+          {loader: "css-loader", options: {url: false}},
           'sass-loader'
         ],
       },
