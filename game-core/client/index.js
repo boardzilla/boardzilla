fa3a776075d28ddf8ac384371756d1330156c708
Joshua Hull <joshbuddy@gmail.com>
2022-04-16 23:48:35 +0200

    add to client

--- game-core/client/index.js
+++ game-core/client/index.js
@@ -1,6 +1,14 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Page from './Page'
+
+Sentry.init({
+  dsn: "https://c149a1d2a5464aae80d74fddcb7f1f1a@o1206738.ingest.sentry.io/6340273",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 export const render = ({pieces, counters, background}) => ReactDOM.render(
   <Page
