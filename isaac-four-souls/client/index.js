4b2cc2884b3499e5ce8613cd6bb05d1d7c0977ce
Joshua Hull <joshbuddy@gmail.com>
2022-05-25 15:06:43 +0200

    Add lowres images

--- isaac-four-souls/client/index.js
+++ isaac-four-souls/client/index.js
@@ -24,7 +24,7 @@ render({
   pieces: {
     card: props => (
       <div>
-        <img src={`images/cards/${props.id ? props.front : props.back}`} />
+        <img lowsrc={props.frontLow || props.back} src={`images/cards/${props.id ? props.front : props.back}`} />
         {props.children}
       </div>
     )
